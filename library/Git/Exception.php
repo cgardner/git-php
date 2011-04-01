96732885d02bccc9ecaaf478be9d62f7aad36b8b
Craig Gardner <Craig_Gardner@adp.com>
2011-04-01 15:57:04 -0700

    added the Git_Exception class

---
+++ library/Git/Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Git Exception
+ * @package Git
+ * @author Craig Gardner <craig_gardner@adp.com>
+ **/
+class Git_Exception extends Exception {
+    /**
+     * Public Methods |publics
+     */
+    
+
+} // end class Git_Exception extends Exception

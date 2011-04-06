f80e5d24e8dc86d491f4f0a405a3f2b5f002e52f
Craig Gardner <Craig_Gardner@adp.com>
2011-04-06 16:41:52 -0700

    added empty classes for Git_Index, Git_Repository, and Git_WorkingDirectory

---
+++ library/Git/Index.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'Git/Path.php';
+
+/**
+ * Git Index Class
+ * @package Git
+ * @author Craig Gardner <craig_gardner@adp.com>
+ **/
+class Git_Index extends Git_Path {
+    /**
+     * Public Methods |publics
+     */
+    
+
+} // end class Git_Index extends Git_Path
+?>
---
+++ library/Git/Repository.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'Git/Path.php';
+
+/**
+ * Git Repository Class
+ * @package Git
+ * @author Craig Gardner <craig_gardner@adp.com>
+ **/
+class Git_Repository extends Git_Path {
+    /**
+     * Public Methods |publics
+     */
+} // end class Git_Repository extends Git_Path
+?>

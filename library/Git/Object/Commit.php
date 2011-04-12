d1c1dae1821767736037d7981f9e8ecec4cdb00b
config_4da495d7327cd <Craig_Gardner@adp.com>
2011-04-12 12:40:38 -0700

    added skeleton for Git_Object_Commit

---
+++ library/Git/Object/Commit.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Commit Object
+ * @package Git
+ * @author Craig Gardner <craig_gardner@adp.com>
+ **/
+class Git_Object_Commit extends Git_Object_Abstract {
+    /**
+     * Public Methods |publics
+     */
+    
+
+} // end class Git_Object_Commit extends Git_Object_Abstract

08f97f649c27a388927d2580e248a6ba5fdf2523
config_4da495d7327cd <Craig_Gardner@adp.com>
2011-04-12 12:40:56 -0700

    updated and added tests for Git_Object class

--- library/Git/Object.php
+++ library/Git/Object.php
@@ -1,5 +1,9 @@
 <?php
 require_once 'Git/Object/Abstract.php';
+require_once 'Git/Object/Tag.php';
+require_once 'Git/Object/Tree.php';
+require_once 'Git/Object/Blob.php';
+require_once 'Git/Object/Commit.php';
 
 /**
  * Git Object Class
@@ -32,23 +36,18 @@ class Git_Object extends Git_Object_Abstract {
         $type = $base->getLib()->objectType($objectish);
         switch ($type) {
             case 'blob':
-                $klass = 'Blob';
+                $klass = 'Git_Object_Blob';
                 break;
 
             case 'commit':
-                $klass = 'Commit';
+                $klass = 'Git_Object_Commit';
                 break;
 
             case 'tree':
-                $klass = 'Tree';
+                $klass = 'Git_Object_Tree';
                 break;
-
-            default:
-                throw new Git_Exception(sprintf('Object Type %s does not exist', $type));
         }
 
         return new $klass($base, $objectish);
     } // end function factory
-    
-
 } // end class Git_Object extends Git_Object_Abstract
---
+++ tests/Git/Object.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Test class for Git_Object
+ * @package Git
+ * @author Craig Gardner <craig_gardner@adp.com>
+ **/
+class Test_Git_Object extends Test_Git_BaseTest {
+    /**
+     * Public Methods |publics
+     */
+    /**
+     * Test the factory method
+     * @param array $params
+     * @param string $expected
+     * @return void
+     * @author Craig Gardner <craig_gardner@adp.com>
+     * @group all
+     * @covers Git_Object::factory
+     * @dataProvider factoryDataProvider
+     **/
+    public function testFactory(array $params, $expected) {
+        array_unshift($params, $this->base);
+        $object = call_user_func_array('Git_Object::factory', $params);
+        $this->assertInstanceOf($expected, $object);
+    } // end function testFactory
+
+    /**
+     * Make sure the Factory method throws a 
+     * Git_Object_Exception_TagDoesNotExist exception when it is supposed to
+     * @param void
+     * @return void
+     * @author Craig Gardner <craig_gardner@adp.com>
+     * @group all
+     * @covers Git_Object::factory
+     * @expectedException Git_Exception_Execute
+     **/
+    public function testFactoryTagDoesNotExistException() {
+        $object = Git_Object::factory($this->base, 'tag/does/not/exist', NULL, TRUE);
+    } // end function testFactoryTagDoesNotExistException
+
+    /**
+     * Data Providers
+     */
+    /**
+     * Data Provider for testFactory
+     * @param void
+     * @return array
+     * @author Craig Gardner <craig_gardner@adp.com>
+     **/
+    public function factoryDataProvider() {
+        return array(
+            'tag' => array(
+                array(
+                    'test',
+                    'tag',
+                    TRUE
+                ),
+                'Git_Object_Tag',
+            ),
+            'commit' => array(
+                array(
+                    'HEAD',
+                ),
+                'Git_Object_Commit',
+            ),
+            'tree' => array(
+                array(
+                    'HEAD^{tree}',
+                ),
+                'Git_Object_Tree',
+            ),
+            'blob' => array(
+                array(
+                    'HEAD:build.properties',
+                ),
+                'Git_Object_Blob',
+            ),
+        );
+        
+    } // end function factoryDataProvider
+
+} // end class Test_Git_Object extends Test_Git_BaseTest
+?>

1d675c7d076d7e700a0c3d7cde67ea7e8333fb29
Craig Gardner <Craig_Gardner@adp.com>
2011-04-12 11:11:17 -0700

    defined a constant that refers to the projects base directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,15 @@
 <?php
 
+defined('BASE_DIR') ||
+    define('BASE_DIR', $this->getProject()->getProperty('project.basedir'));
 if (defined('LIBRARY_PATH') === FALSE) {
-    $libraryPath = realpath($this->getProject()->getProperty('project.basedir') .'/library');
+    $libraryPath = realpath(BASE_DIR .'/library');
     if (!$libraryPath) {
         throw new Exception('Library Path does not exist');
     }
     define('LIBRARY_PATH', $libraryPath);
 }
+
 
 set_include_path(implode(PATH_SEPARATOR, array(
     LIBRARY_PATH,
